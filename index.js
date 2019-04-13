9ae7d329af9a3fec1d4f7766a6be51d12c2ad3a0
joogps <joaogabrielps13@hotmail.com>
2019-04-12 23:50:36 -0300

    Preventing crashes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const io = require('socket.io-client')('https://control-leds.herokuapp.com/');
 const gpio = require('rpi-gpio');
 
 gpio.setMode(gpio.MODE_BCM);
-gpio.setup(4, gpio.DIR_OUT);
+
+const pins = [4];
+for (let pin of pins)
+	gpio.setup(pin, gpio.DIR_OUT);
 
 io.on('action', function(data) {
-	gpio.write(data.pin, data.state=='on');
-})
+	if (pins.includes(data.pin) && ['on', 'off'].includes(data.state))
+		gpio.write(data.pin, data.state=='on');
+});
