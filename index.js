0209c0e6ce593a5f3ac75356235f646774b120f8
joogps <joaogabrielps13@hotmail.com>
2019-04-12 22:34:03 -0300

    Changing the way pins are set up

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const io = require('socket.io-client')('https://control-leds.herokuapp.com/');
 const gpio = require('rpi-gpio');
 
 gpio.setMode(gpio.MODE_BCM);
+gpio.setup(4, gpio.DIR_OUT);
 
 io.on('action', function(data) {
-	gpio.setup(data.pin, gpio.DIR_OUT);
-	gpio.write(data.pin, data.state='on');
+	gpio.write(data.pin, data.state=='on');
 })
